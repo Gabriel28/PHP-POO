5e72aac2ed588060a758198bef2189d6cfa852a1
Gabriel28 <nasc.gabriel.1997@gmail.com>
2019-12-31 17:47:15 -0300

    Mudança para raiz

---
+++ Operacoes.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'php bank/src/Conta.php';
+require_once 'php bank/src/Titular.php';
+require_once 'php bank/src/CPF.php';
+
+$debConta = new Conta(new Titular(new CPF("132.987.126-69"), "TAYSON"));
+$gabConta = new Conta(new Titular(new CPF("123.123.433-98"), "SEIYA"));
+
+$gabConta -> depositar(3000);
+
+echo $gabConta-> recuperaNomeTitular() . PHP_EOL;
+echo $gabConta-> recuperaCpfTitular() . PHP_EOL;
+
+$gabConta -> transfere(2000, $debConta);
+
+echo $debConta-> informaSaldo();
+
+
