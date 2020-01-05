a2d2431ce69bc933c4aaafb20268683d096c336f
Gabriel28 <nasc.gabriel.1997@gmail.com>
2020-01-05 11:50:20 -0300

    add metódo de recuperacao do titular

--- php_bank/src/Titular.php
+++ php_bank/src/Titular.php
@@ -4,14 +4,17 @@ class Titular
 {
     private CPF $cpf;
     private string $nome;
+    private string $endereco;
 
-    public function __construct(CPF $cpf, string $nome)
+    public function __construct(CPF $cpf, string $nome, string $endereco)
     {
         $this->cpf = $cpf;
         $this->validaNomeTitular($nome);
         $this->nome = $nome;
+        $this->endereco = $endereco;
     }
 
+    //Realiza a validação do nome
     private function validaNomeTitular(string $nomeTitular)
     {
         if (strlen($nomeTitular) < 5) {
@@ -20,13 +23,22 @@ class Titular
         }
     }
 
+    //Recupera o CPF do usuário
     public function recuperaCpf(): string
     {
         return $this->cpf->recuperaNumero();
     }
 
+    //Recupera o nome do usuário
     public function recuperaNome(): string
     {
         return $this->nome;
     }
+
+    //Retorna o endereço do usuário
+    public function recuperaEnd(): string
+    {
+        return $this->endereco;
+    }
+
 }
