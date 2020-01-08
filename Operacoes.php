fed09a6367c6d7d63e41eec465c82001ab1fe1b1
Gabriel28 <nasc.gabriel.1997@gmail.com>
2020-01-08 20:09:26 -0300

    Alterações para teste

--- Operacoes.php
+++ Operacoes.php
@@ -1,11 +1,14 @@
 <?php
 
 require_once 'php_bank/src/Conta.php';
+require_once 'php_bank/src/Endereco.php';
 require_once 'php_bank/src/Titular.php';
 require_once 'php_bank/src/CPF.php';
 
-$debConta = new Conta(new Titular(new CPF("132.987.126-69"), "TAYSON", "Fuentes, 123. Col"));
-$gabConta = new Conta(new Titular(new CPF("123.123.433-98"), "SEIYA", "Avellaneda"));
+$endereco = new Endereco ('São Paulo', 'Suzano', 'Rua forte', '71');
+$endereco2 = new Endereco ('São Carlos', 'Centro', 'Rua presidente', '174');
+$debConta = new Conta(new Titular(new CPF("132.987.126-69"), "TAYSON", $endereco2));
+$gabConta = new Conta(new Titular(new CPF("123.123.433-98"), "SEIYA", $endereco));
 
 $gabConta -> depositar(3000);
 
@@ -16,6 +19,5 @@ echo $gabConta-> recuperaCpfTitular() . PHP_EOL;
 $gabConta -> transfere(2000, $debConta);
 
 echo $debConta-> informaSaldo() . PHP_EOL;
-echo $debConta-> recuperaEndTitular() . PHP_EOL;
 
 var_dump($debConta);
