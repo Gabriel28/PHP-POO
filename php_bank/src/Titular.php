b0fe2152e6a94c8c7c361ac27baf422401d821b9
Gabriel28 <nasc.gabriel.1997@gmail.com>
2020-01-08 21:38:24 -0300

    Classe titular modificada herdando classe Pessoa

--- php_bank/src/Titular.php
+++ php_bank/src/Titular.php
@@ -1,38 +1,14 @@
 <?php
 
-class Titular
+class Titular extends Pessoa
 {
-    private CPF $cpf;
-    private string $nome;
     private Endereco $endereco;
 
     public function __construct(CPF $cpf, string $nome, Endereco $endereco)
     {
-        $this->cpf = $cpf;
+        parent::__construct($nome, $cpf);
         $this->validaNomeTitular($nome);
-        $this->nome = $nome;
         $this->endereco = $endereco;
-    }
-
-    //Realiza a validação do nome
-    private function validaNomeTitular(string $nomeTitular)
-    {
-        if (strlen($nomeTitular) < 5) {
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        }
-    }
-
-    //Recupera o CPF do usuário
-    public function recuperaCpf(): string
-    {
-        return $this->cpf->recuperaNumero();
-    }
-
-    //Recupera o nome do usuário
-    public function recuperaNome(): string
-    {
-        return $this->nome;
     }
 
     //Retorna o endereço do usuário
