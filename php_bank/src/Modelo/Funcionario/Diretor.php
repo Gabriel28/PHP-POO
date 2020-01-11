9db700ff94b493fd4c84ce20bfb3c69bd89bac13
Gabriel28 <nasc.gabriel.1997@gmail.com>
2020-01-10 21:25:26 -0300

    Criação da distinção de funcionários

---
+++ php_bank/src/Modelo/Funcionario/Diretor.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Alura\Banco\Modelo\Funcionario;
+
+class Diretor extends Funcionario
+{
+    public function calculaBonificacao(): float
+    {
+        return $this->recuperaSalario() * 2;
+    }
+}
